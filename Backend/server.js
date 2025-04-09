f9a850f4ffc381598729f50a2815b1262dd020dc
Diksha <116185338+diksha-34@users.noreply.github.com>
2025-04-10 00:28:52 +0530

    updated dotenv import

--- Backend/server.js
+++ Backend/server.js
@@ -2,14 +2,13 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./config/db');
-
-
 dotenv.config();
 
 
-const app = express();
+
 
 app.use(express.json());
+const app = express();
 app.use(cors());
 
 connectDB();
