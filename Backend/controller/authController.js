31ab4144de52ae004e428974ffc4025b0736b03d
Diksha <116185338+diksha-34@users.noreply.github.com>
2025-04-13 16:55:42 +0530

    updated

--- Backend/controller/authController.js
+++ Backend/controller/authController.js
@@ -1,6 +1,6 @@
 const User = require('../model/userModel');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 exports.register = async (req, res) => {
     const { name, email, password} = req.body;
