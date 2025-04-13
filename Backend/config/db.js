310f5d504c7a5f0f2af7c302b86ee2597db7454b
Diksha <116185338+diksha-34@users.noreply.github.com>
2025-04-13 16:14:15 +0530

    updated changes

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 const connectDb = async () =>{
     try{
-        console.log(process.env.MONGO_URI)
+        if(!process.env.MONGO_URI)
+            throw new error("got mongo uri undefined")
+     
         await mongoose.connect(process.env.MONGO_URI);
         console.log('MongoDB connected successfully');
 
